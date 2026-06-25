93dfa68cbb5492545976ff1f116c7a61a20976eb
HarshitVanani <harshitvanani@gmail.com>
2026-06-26 01:08:31 +0530

    fix: remove legacy next callback from async pre-save hook

--- backend/models/User.js
+++ backend/models/User.js
@@ -28,19 +28,15 @@ const UserSchema = new mongoose.Schema({
     }
 }, { 
     timestamps: true,
-    autoIndex: true // Forces Mongoose to rebuild/sync mismatched database indexes on startup
+    autoIndex: true 
 });
 
-// AUTOMATIC PASSWORD HASHING HOOK
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
+// FIXED: Clean async/await hook without 'next' callback parameter
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('User', UserSchema);
